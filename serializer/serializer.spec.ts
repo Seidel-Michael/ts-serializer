655de84b2fc68d0e200817793df551216e80941f
Michael Seidel <github@seidel-michael.de>
2018-05-14 17:49:43 +0200

    serialize to object or file implemented

--- serializer/serializer.spec.ts
+++ serializer/serializer.spec.ts
@@ -3,12 +3,11 @@
 import * as chai from 'chai';
 import {expect} from 'chai';
 import * as chaiAsPromised from 'chai-as-promised';
-
 import * as fs from 'fs-extra';
 import * as mockFS from 'mock-fs';
 import * as sinon from 'sinon';
 
-import {SerializedObjectIncompleteError, FileNotFoundError,FileReadError, FileParseError} from './errors';
+import {FileNotFoundError, FileParseError, FileReadError, FileWriteError, SerializedObjectIncompleteError} from './errors';
 import {Serializable} from './serializable';
 import {Serializer} from './serializer';
 
@@ -117,55 +116,134 @@ describe('Serializer', () => {
     let stub = null;
 
     afterEach(() => {
-        if(stub)
-        {
-            stub.restore();
-            stub = null;
-        }
+      if (stub) {
+        stub.restore();
+        stub = null;
+      }
 
-        mockFS.restore();
+      mockFS.restore();
     });
 
     it('should reject with FileNotFoundError if the given file does not exist', () => {
-        mockFS({});
+      mockFS({});
 
-        return expect(Serializer.deserializeFile<TestClassNoMandatoryNoExclude>(TestClassNoMandatoryNoExclude, '/testFolder/testFile.json'))
-        .to.be.rejectedWith(FileNotFoundError);
+      return expect(Serializer.deserializeFile<TestClassNoMandatoryNoExclude>(TestClassNoMandatoryNoExclude, '/testFolder/testFile.json'))
+          .to.be.rejectedWith(FileNotFoundError);
     });
 
     it('should reject with FileReadError if the given file could not be read', () => {
-        mockFS({
-            '/testFolder/testFile.json': mockFS.file({
-              content: JSON.stringify({test: 'cde'}),
-            }),
-          });
+      mockFS({
+        '/testFolder/testFile.json': mockFS.file({
+          content: JSON.stringify({test: 'cde'}),
+        }),
+      });
 
-          stub = sinon.stub(fs, 'readJson').rejects(Error);
+      stub = sinon.stub(fs, 'readJson').rejects(Error);
 
-        return expect(Serializer.deserializeFile<TestClassNoMandatoryNoExclude>(TestClassNoMandatoryNoExclude, '/testFolder/testFile.json'))
-        .to.be.rejectedWith(FileReadError);
+      return expect(Serializer.deserializeFile<TestClassNoMandatoryNoExclude>(TestClassNoMandatoryNoExclude, '/testFolder/testFile.json'))
+          .to.be.rejectedWith(FileReadError);
     });
 
     it('should reject with FileParseError if the given file does not contain valid json', () => {
-        mockFS({
-            '/testFolder/testFile.json': mockFS.file({
-              content: '<xml></xml>',
-            })
-          });
+      mockFS({
+        '/testFolder/testFile.json': mockFS.file({
+          content: '<xml></xml>',
+        })
+      });
 
-        return expect(Serializer.deserializeFile<TestClassNoMandatoryNoExclude>(TestClassNoMandatoryNoExclude, '/testFolder/testFile.json'))
-        .to.be.rejectedWith(FileParseError);
+      return expect(Serializer.deserializeFile<TestClassNoMandatoryNoExclude>(TestClassNoMandatoryNoExclude, '/testFolder/testFile.json'))
+          .to.be.rejectedWith(FileParseError);
     });
 
     it('should resolve with deserialized object if file is ok', () => {
-        mockFS({
-            '/testFolder/testFile.json': mockFS.file({
-              content: JSON.stringify({test: 'cde'}),
-            }),
-          });
+      mockFS({
+        '/testFolder/testFile.json': mockFS.file({
+          content: JSON.stringify({test: 'cde'}),
+        }),
+      });
 
-        return expect(Serializer.deserializeFile<TestClassNoMandatoryNoExclude>(TestClassNoMandatoryNoExclude, '/testFolder/testFile.json'))
-        .to.eventually.include({test: 'cde'});
+      return expect(Serializer.deserializeFile<TestClassNoMandatoryNoExclude>(TestClassNoMandatoryNoExclude, '/testFolder/testFile.json'))
+          .to.eventually.include({test: 'cde'});
+    });
+  });
+
+  describe('serialize', () => {
+    it('should resolve with valid serialized object - no exclude', () => {
+      const object = new TestClassMandatoryNoExclude();
+      object.mandatoryProperty = 'test';
+
+      return expect(Serializer.serialize<TestClassMandatoryNoExclude>(object)).to.eventually.deep.equal({test: 'Test123', mandatoryProperty: 'test'});
+    });
+
+    it('should resolve with valid serialized object - exclude', () => {
+      const object = new TestClassMandatoryExclude();
+      object.mandatoryProperty = 'test';
+      object.excludedProperty = 'abc test';
+
+      return expect(Serializer.serialize<TestClassMandatoryExclude>(object)).to.eventually.deep.equal({test: 'Test123', mandatoryProperty: 'test'});
+    });
+  });
+
+  describe('serializeFile', () => {
+    let stub = null;
+
+    afterEach(() => {
+      if (stub) {
+        stub.restore();
+        stub = null;
+      }
+
+      mockFS.restore();
+    });
+
+    it('should create new file if the file does not exist', async () => {
+      mockFS({});
+
+      const testObject = new TestClassNoMandatoryNoExclude();
+
+      await Serializer.serializeFile<TestClassNoMandatoryNoExclude>(testObject, '/testFolder/testFile.json');
+
+      expect(fs.readJsonSync('/testFolder/testFile.json')).to.deep.equal({test: 'Test123'});
+    });
+
+    it('should overwrite file if the file does exist', async () => {
+      mockFS({
+        '/testFolder/testFile.json': mockFS.file({
+          content: JSON.stringify({test: 'cde'}),
+        }),
+      });
+
+      const testObject = new TestClassNoMandatoryNoExclude();
+
+      await Serializer.serializeFile<TestClassNoMandatoryNoExclude>(testObject, '/testFolder/testFile.json');
+
+      expect(fs.readJsonSync('/testFolder/testFile.json')).to.deep.equal({test: 'Test123'});
+    });
+
+    it('should reject with FileWriteError if an error ocurred while creating file or folder', () => {
+      mockFS({});
+
+      stub = sinon.stub(fs, 'ensureFile').rejects(Error);
+
+      const testObject = new TestClassNoMandatoryNoExclude();
+
+      return expect(Serializer.serializeFile<TestClassNoMandatoryNoExclude>(testObject, '/testFolder/testFile.json'))
+          .to.be.rejectedWith(FileWriteError);
+    });
+
+    it('should reject with FileWriteError if an error ocurred while writing', () => {
+      mockFS({
+        '/testFolder/testFile.json': mockFS.file({
+          content: JSON.stringify({test: 'cde'}),
+        }),
+      });
+
+      stub = sinon.stub(fs, 'writeJson').rejects(Error);
+
+      const testObject = new TestClassNoMandatoryNoExclude();
+
+      return expect(Serializer.serializeFile<TestClassNoMandatoryNoExclude>(testObject, '/testFolder/testFile.json'))
+          .to.be.rejectedWith(FileWriteError);
     });
   });
 });
--- serializer/serializer.ts
+++ serializer/serializer.ts
@@ -1,6 +1,6 @@
 import * as fs from 'fs-extra';
 
-import {FileNotFoundError, FileParseError, FileReadError, SerializedObjectIncompleteError} from './errors';
+import {FileNotFoundError, FileParseError, FileReadError, FileWriteError, SerializedObjectIncompleteError} from './errors';
 import {Serializable} from './serializable';
 
 /**
@@ -68,7 +68,7 @@ export class Serializer {
    * FileParseError if the file content is not valid json.
    * @memberof Serializer
    */
-  static deserializeFile<T>(type: any, file: any): Promise<T> {
+  static deserializeFile<T extends Serializable>(type: any, file: any): Promise<T> {
     return new Promise<T>((resolve, reject) => {
 
       if (!fs.existsSync(file)) {
@@ -89,6 +89,66 @@ export class Serializer {
           .then((serializedData) => {
             this.deserialize<T>(type, serializedData).then(resolve).catch(reject);
           });
+    });
+  }
+
+  /**
+   * Serializes the object to serialized data.
+   *
+   * @static
+   * @template T The expected returning type for compile time type definition.
+   * @param {T} object The object to serialize.
+   * @returns {Promise<any>} Returns a promise resolving with the serialized data.
+   * @memberof Serializer
+   */
+  static serialize<T extends Serializable>(object: T): Promise<any> {
+    return new Promise<any>((resolve, reject) => {
+      // Init empty array
+      if (!object['_serializable_nonserialized']) {
+        object['_serializable_nonserialized'] = [];
+      }
+
+      const serializedData: any = {};
+
+      // Iterate properties
+      for (const property in object) {
+        if (!object['_serializable_nonserialized'].includes(property) && !property.startsWith('_serializable_')) {
+          serializedData[property] = object[property];
+        }
+      }
+
+      resolve(serializedData);
+    });
+  }
+
+  /**
+   * Serialized the object to a file.
+   *
+   * @static
+   * @template T The expected returning type for compile time type definition.
+   * @param {T} object The object to serialize.
+   * @param {*} file The path to the file to serialize to. Not existing files and folders will be created.
+   * @returns {Promise<void>} Returns a promise resolving if the file was written successfully.
+   * The promise will be rejected with a FileWriteError if anything went wrong during writing or creating the folders and file.
+   * @memberof Serializer
+   */
+  static serializeFile<T extends Serializable>(object: T, file: any): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+      this.serialize(object).then((serializedData) => {
+
+        fs.ensureFile(file)
+            .catch((error) => {
+              reject(new FileWriteError(file, error));
+            })
+            .then(() => {
+              fs.writeJson(file, serializedData)
+                  .catch((error) => {
+                    reject(new FileWriteError(file, error));
+                  })
+                  .then(resolve);
+            });
+
+      });
     });
   }
 }
